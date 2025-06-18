044adb7cd7b2a29b7872670a62bda7698ff95c03
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-06-18 20:56:57 +0700

    feat(03): add dependency

---
+++ 03 - golang modules/go.mod
@@ -0,0 +1,5 @@
+module golang-modules
+
+go 1.23.3
+
+require github.com/canonflow/golang-pzn-modules v1.0.0 // indirect
---
+++ 03 - golang modules/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	golang_pzn_modules "github.com/canonflow/golang-pzn-modules"
+)
+
+func main() {
+	fmt.Println(golang_pzn_modules.SayHello()) // Hello
+}
