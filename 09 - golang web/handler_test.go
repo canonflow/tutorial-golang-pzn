45903ee84d864f4304a3b64f8b1249e60ca06936
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-06 22:36:50 +0700

    feat(09): request

--- 09 - golang web/handler_test.go
+++ 09 - golang web/handler_test.go
@@ -51,3 +51,20 @@ func TestServeHTTP(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writter, request.Method)
+		fmt.Fprintln(writter, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
