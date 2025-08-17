9df2eaca313a26766fd58e434f230122ab0bab04
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-17 23:22:22 +0700

    feat(09): download file

---
+++ 09 - golang web/download_file_test.go
@@ -0,0 +1,32 @@
+package _9___golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func DownloadFile(writer http.ResponseWriter, request *http.Request) {
+	file := request.URL.Query().Get("file")
+
+	if file == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(writer, "Bad Request")
+		return
+	}
+
+	writer.Header().Add("Content-Disposition", "attachment; filename=\""+file+"\"")
+	http.ServeFile(writer, request, "./resources/"+file)
+}
+
+func TestDownloadFile(t *testing.T) {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(DownloadFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
