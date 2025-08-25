86fe326440927f8520077507d6ab4ed5d19b98e3
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-25 20:51:17 +0700

    feat(12): http server

--- 12 - golang restful api/main.go
+++ 12 - golang restful api/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 	"goalng-restful-api/app"
 	"goalng-restful-api/controller"
+	"goalng-restful-api/helper"
 	"goalng-restful-api/repository"
 	"goalng-restful-api/service"
+	"net/http"
 )
 
 func main() {
@@ -24,4 +27,12 @@ func main() {
 	router.POST("/api/categories", categoryController.Create)
 	router.PUT("/api/categories/:categoryId", categoryController.Update)
 	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
 }
