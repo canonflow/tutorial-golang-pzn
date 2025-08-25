975340e353399de6590764358ae17ee527087ba3
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-25 20:02:49 +0700

    feat(12): category validation

--- 12 - golang restful api/service/category_service_impl.go
+++ 12 - golang restful api/service/category_service_impl.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"database/sql"
+	"github.com/go-playground/validator/v10"
 	"goalng-restful-api/helper"
 	"goalng-restful-api/model/domain"
 	"goalng-restful-api/model/web"
@@ -12,9 +13,13 @@ import (
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer func() {
@@ -32,6 +37,9 @@ func (service CategoryServiceImpl) Create(ctx context.Context, request web.Categ
 }
 
 func (service CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer func() {
