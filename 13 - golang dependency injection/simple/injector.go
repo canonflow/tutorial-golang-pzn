e78c8847f388e175e0cc475cbddc046d204d1ede
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-27 21:29:34 +0700

    feat(13): injector paramter

--- 13 - golang dependency injection/simple/injector.go
+++ 13 - golang dependency injection/simple/injector.go
@@ -5,7 +5,11 @@ package simple
 
 import "github.com/google/wire"
 
-func InitializeService() (*SimpleService, error) {
+func InitializeService(isError bool) (*SimpleService, error) {
+	/*
+		Parameter isError akan dimasukkan sebagai argument dari NewSimpleRepository karena
+		NewSimpleRepository memeliki parameter boolean dan isError memiliki tipe boolean (sama)
+	*/
 	wire.Build(
 		NewSimpleRepository,
 		NewSimpleService,
--- 13 - golang dependency injection/test/simple_service_test.go
+++ 13 - golang dependency injection/test/simple_service_test.go
@@ -1,1 +1,29 @@
 package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"golang-dependency-injection/simple"
+	"testing"
+)
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleService, err := simple.InitializeService(false)
+	assert.NotNil(t, simpleService)
+	assert.Nil(t, err)
+	/*
+		=== RUN   TestSimpleServiceSuccess
+		--- PASS: TestSimpleServiceSuccess (0.00s)
+		PASS
+	*/
+}
+
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializeService(true)
+	assert.Nil(t, simpleService)
+	assert.NotNil(t, err)
+	/*
+		=== RUN   TestSimpleServiceError
+		--- PASS: TestSimpleServiceError (0.00s)
+		PASS
+	*/
+}
