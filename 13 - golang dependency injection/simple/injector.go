b473e88c848524c50853f38f75023c52ebb116f5
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-08-28 20:13:14 +0700

    feat(13): provider set

--- 13 - golang dependency injection/simple/injector.go
+++ 13 - golang dependency injection/simple/injector.go
@@ -25,3 +25,12 @@ func InitializeDatabaseRepository() *DatabaseRepository {
 	)
 	return nil
 }
+
+// Hanya Groping antar Provider
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
+
+func InitializeFooBarService() *FooBarService {
+	wire.Build(fooSet, barSet, NewFooBarService)
+	return nil
+}
