1b5b570fbcf6350a97b701de526529a7b1fe7817
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-09-08 20:35:03 +0700

    feat(17): validation basic collection

--- 17 - golang validation/03-validation_nested_test.go
+++ 17 - golang validation/03-validation_nested_test.go
@@ -84,3 +84,49 @@ func TestCollection(t *testing.T) {
 		*/
 	}
 }
+
+func TestBasicCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id        string    `validate:"required"`
+		Name      string    `validate:"required"`
+		Addresses []Address `validate:"required,dive"`
+		Hobbies   []string  `validate:"required,dive,required,min=3"`
+	}
+
+	validate := validator.New()
+
+	request := User{
+		Id:   "",
+		Name: "",
+		Addresses: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+		Hobbies: []string{"Swimming", "Eating", "X"},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+		/*
+			Key: 'User.Id' Error:Field validation for 'Id' failed on the 'required' tag
+			Key: 'User.Name' Error:Field validation for 'Name' failed on the 'required' tag
+			Key: 'User.Addresses[0].City' Error:Field validation for 'City' failed on the 'required' tag
+			Key: 'User.Addresses[0].Country' Error:Field validation for 'Country' failed on the 'required' tag
+			Key: 'User.Addresses[1].City' Error:Field validation for 'City' failed on the 'required' tag
+			Key: 'User.Addresses[1].Country' Error:Field validation for 'Country' failed on the 'required' tag
+			Key: 'User.Hobbies[2]' Error:Field validation for 'Hobbies[2]' failed on the 'min' tag
+		*/
+	}
+}
