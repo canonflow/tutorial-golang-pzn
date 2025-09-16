8b91feb1f5cc362f991a2b58c6044247d05a954e
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-09-16 20:19:37 +0700

    feat(19): ctx

--- 19 - golang fiber/fiber_test.go
+++ 19 - golang fiber/fiber_test.go
@@ -32,3 +32,27 @@ func TestRoutingHello(t *testing.T) {
 		PASS
 	*/
 }
+
+var app = fiber.New()
+
+func TestCtx(t *testing.T) {
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/?name=Nathan", nil)
+
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Nathan", string(bytes))
+	/*
+		=== RUN   TestCtx
+		--- PASS: TestCtx (0.00s)
+		PASS
+	*/
+}
