1bbc428f99017cb238db84594fade128dc3df9b4
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-09-17 21:40:35 +0700

    feat(19): prefork

--- 19 - golang fiber/main.go
+++ 19 - golang fiber/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +12,14 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
+		Prefork:      true, // Enable prefork mode
 	})
+
+	if fiber.IsChild() {
+		fmt.Println("This is a child process")
+	} else {
+		fmt.Println("This is the parent process")
+	}
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World!")
