579c7ab5e571d90f5c3c11f52bb127c1b91e1d9a
Nathan Garzya Santoso <nathangarzyasantoso7@gmail.com>
2025-09-17 21:58:54 +0700

    feat(19): middleware

--- 19 - golang fiber/main.go
+++ 19 - golang fiber/main.go
@@ -15,14 +15,35 @@ func main() {
 		Prefork:      true, // Enable prefork mode
 	})
 
+	//! PREFORK
 	if fiber.IsChild() {
 		fmt.Println("This is a child process")
 	} else {
 		fmt.Println("This is the parent process")
 	}
 
+	//! MIDDLEWARE
+	app.Use(func(c *fiber.Ctx) error {
+		fmt.Println("Request received at:", time.Now())
+		err := c.Next()
+		fmt.Println("Response sent at:", time.Now())
+		return err
+	})
+
+	//! MIDDLEWARE ON SPECIFIC ROUTE PREFIX
+	app.Use("/api", func(c *fiber.Ctx) error {
+		fmt.Println("API Request received at:", time.Now())
+		err := c.Next()
+		fmt.Println("API Response sent at:", time.Now())
+		return err
+	})
+
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello World!")
+	})
+
+	app.Get("/api/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from API!")
 	})
 
 	err := app.Listen("localhost:3000")
